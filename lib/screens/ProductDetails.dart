c1d97e87064bf1e472c607711c8d6412896eabfc
ba-lambert <mugwanezalambert12b@gmail.com>
2024-02-27 15:40:17 +0200

    final details page

--- lib/screens/ProductDetails.dart
+++ lib/screens/ProductDetails.dart
@@ -13,6 +13,7 @@ class ProductDetails extends StatefulWidget {
 
 class _ProductDetailsState extends State<ProductDetails> {
   bool showFullDescription = false;
+  int imageIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,7 +28,7 @@ class _ProductDetailsState extends State<ProductDetails> {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   GestureDetector(
-                    onTap: () {},
+                    onTap: () => Navigator.pop(context),
                     child: SvgPicture.asset(
                       'assets/icons/arrowback_ic.svg',
                       height: 30,
@@ -51,97 +52,180 @@ class _ProductDetailsState extends State<ProductDetails> {
               ),
             ),
           )),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20.w),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Center(
-                child: SizedBox(
-                  height: 250.h,
-                  child: Image.asset(
-                    widget.product['photos'][0],
-                    height: 500.h,
-                  ),
-                ),
-              ),
-              Text(
-                'Best Seller'.toUpperCase(),
-                style: GoogleFonts.montserrat(
-                  color: Colors.lightBlue,
-                  fontWeight: FontWeight.w500,
-                  fontSize: 20,
-                ),
-              ),
-              Text(
-                widget.product['title'],
-                style: GoogleFonts.montserrat(
-                  fontSize: 25.sp,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              SizedBox(
-                height: 10.h,
-              ),
-              Text(
-                widget.product['price'],
-                style: GoogleFonts.montserrat(
-                  fontSize: 18.sp,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(
-                height: 10.h,
-              ),
-              Text(
-                showFullDescription
-                    ? widget.product['description']
-                    : _truncateDescription(widget.product['description']),
-                style: GoogleFonts.montserrat(color: Colors.grey),
-              ),
-              if (widget.product['description'].split(' ').length > 50)
-                TextButton(
-                  onPressed: () {
-                    setState(() {
-                      showFullDescription = !showFullDescription;
-                    });
-                  },
-                  child: Text(
-                    showFullDescription ? 'Read Less' : 'Read More',
-                    style: TextStyle(color: Colors.blue),
-                  ),
-                ),
-              SizedBox(
-                height: 10.h,
-              ),
-              Text(
-                'Gallery',
-                style: GoogleFonts.montserrat(
-                    fontWeight: FontWeight.w700, fontSize: 16.sp),
-              ),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: [
-                    for (var imageUrl in widget.product["photos"])
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Image.asset(
-                          imageUrl,
-                          width: 50.w, // Adjust the width of each image
-                          height: 50.h, // Adjust the height of each image
-                          fit: BoxFit.cover,
-                        ),
+      body: Stack(children: [
+        Positioned.fill(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.w),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Center(
+                    child: SizedBox(
+                      height: 250.h,
+                      child: Image.asset(
+                        widget.product['photos'][imageIndex],
+                        height: 500.h,
                       ),
-                  ],
-                ),
+                    ),
+                  ),
+                  Text(
+                    'Best Seller'.toUpperCase(),
+                    style: GoogleFonts.montserrat(
+                      color: Colors.lightBlue,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 20,
+                    ),
+                  ),
+                  Text(
+                    widget.product['title'],
+                    style: GoogleFonts.montserrat(
+                      fontSize: 25.sp,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10.h,
+                  ),
+                  Text(
+                    widget.product['price'],
+                    style: GoogleFonts.montserrat(
+                      fontSize: 18.sp,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10.h,
+                  ),
+                  Text(
+                    showFullDescription
+                        ? widget.product['description']
+                        : _truncateDescription(widget.product['description']),
+                    style: GoogleFonts.montserrat(color: Colors.grey),
+                  ),
+                  if (widget.product['description'].split(' ').length > 50)
+                    TextButton(
+                      onPressed: () {
+                        setState(() {
+                          showFullDescription = !showFullDescription;
+                        });
+                      },
+                      child: Text(
+                        showFullDescription ? 'Read Less' : 'Read More',
+                        style: TextStyle(color: Colors.blue),
+                      ),
+                    ),
+                  SizedBox(
+                    height: 10.h,
+                  ),
+                  Text(
+                    'Gallery',
+                    style: GoogleFonts.montserrat(
+                        fontWeight: FontWeight.w700, fontSize: 16.sp),
+                  ),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                        for (var i = 0;
+                            i < widget.product["photos"].length;
+                            i++)
+                          Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  imageIndex = i;
+                                });
+                              },
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(50),
+                                child: Image.asset(
+                                  widget.product["photos"][i],
+                                  width: 50.w, // Adjust the width of each image
+                                  height:
+                                      50.h, // Adjust the height of each image
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                          ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10.h,
+                  ),
+                  Text(
+                    'Sizes',
+                    style: GoogleFonts.montserrat(
+                      fontSize: 16.sp,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  )
+                ],
               ),
-            ],
+            ),
           ),
         ),
-      ),
+        Positioned(
+            right: 6.w,
+            bottom: 15.h,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Price',
+                      style: GoogleFonts.montserrat(
+                        color: Colors.grey[500],
+                        fontWeight: FontWeight.w700,
+                        fontSize: 16.sp,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5.h,
+                    ),
+                    Text(
+                      widget.product['price'],
+                      style: GoogleFonts.montserrat(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w700,
+                        fontSize: 14.sp,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  width: 130.w,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.blue,
+                    onPrimary: Colors.white,
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 25.w, vertical: 15),
+                    shape: RoundedRectangleBorder(
+                      borderRadius:
+                          BorderRadius.circular(50), // Button border radius
+                      side:
+                          const BorderSide(color: Colors.blue), // Border color
+                    ),
+                    textStyle: GoogleFonts.montserrat(), // Text style
+                  ),
+                  child: Text(
+                    'Add To Cart',
+                    style: GoogleFonts.montserrat(
+                        fontSize: 14.sp, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ))
+      ]),
     );
   }
 
